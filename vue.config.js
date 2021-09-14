60448cb831f45ca134d55191ccdf79785dcc9030
Yuriy Bevov <jury19y@mail.ru>
2021-09-14 14:39:58 +0300

    vue.config path

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
-      ? 'https://yuriybevov.github.io/mcdistest/'
+      ? 'https://yuriybevov.github.io/mcdtest/'
       : '/'
   }
